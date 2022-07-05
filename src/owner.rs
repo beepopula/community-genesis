243cdaee95f3bdc4d6307250ecfb9b18b448c07d
bhc8521 <bhc8521>
2022-07-05 21:27:56 +0800

    feature: deploy by owner

--- src/owner.rs
+++ src/owner.rs
@@ -37,6 +37,33 @@ impl CommunityGenesis {
         let hash = CryptoHash::from(hash).to_vec();
         env::storage_remove(&hash);
     }
+
+    pub fn deploy_community_by_owner(&mut self, name: String, community_type: String, creator_id: AccountId, args: Option<String>) {
+        assert!(self.owner_id == env::predecessor_account_id(), "contract owner only");
+        let code_info = self.codes.get(&community_type).unwrap();
+        let contract_id: AccountId = AccountId::from_str(&(name + "." + &env::current_account_id().to_string())).unwrap();
+        let hash: Vec<u8> = CryptoHash::from(code_info.hash).to_vec();
+        let storage_cost = self.account_storage_usage * env::storage_byte_cost() + u128::from(code_info.storage_deposit);
+
+        assert!(env::attached_deposit() > storage_cost, "not enough deposit");
+
+        Promise::new(contract_id.clone())
+        .create_account()
+        .transfer(u128::from(code_info.storage_deposit) + self.account_storage_usage as u128 * env::storage_byte_cost())
+        .deploy_contract(env::storage_read(&hash).unwrap())
+        .function_call("new".into(), json!({
+            "owner_id": creator_id,
+            "public_key": self.public_key,
+            "args": args 
+        }).to_string().as_bytes().to_vec(), 0, (env::prepaid_gas() - env::used_gas()) / 3).then(
+            Promise::new(env::current_account_id())
+            .function_call("on_add_community".into(), json!({
+                "contract_id": contract_id,
+                "community_type": community_type,
+                "owner_id": creator_id,
+            }).to_string().into(), env::attached_deposit(), (env::prepaid_gas() - env::used_gas()) / 3)
+        );
+    }
 }
 
 #[no_mangle]
